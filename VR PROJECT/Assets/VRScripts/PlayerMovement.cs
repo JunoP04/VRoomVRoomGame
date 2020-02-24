259b635601d64bc215d8735d9d2311dd06948211
Brandon Samaroo <samaroo.bran@gmail.com>
2020-02-23 23:21:02 -0500

    added jumping

--- VR PROJECT/Assets/VRScripts/PlayerMovement.cs
+++ VR PROJECT/Assets/VRScripts/PlayerMovement.cs
@@ -10,7 +10,7 @@ public class PlayerMovement : MonoBehaviour
     public SteamVR_Action_Boolean jump = SteamVR_Input.GetAction<SteamVR_Action_Boolean>("Jump");
     public bool isJumping = false;
     public float speed = 2;
-    public float jumpspeed = 0;
+    public float gravity = -9.81f;
     [SerializeField]private CharacterController characterController;
 
     void Start()
@@ -22,23 +22,20 @@ public class PlayerMovement : MonoBehaviour
     void FixedUpdate()
     {
         Vector3 dirction = Player.instance.hmdTransform.TransformDirection(new Vector3(movement.axis.x, 0, movement.axis.y));
-        characterController.Move(speed * Time.deltaTime * Vector3.ProjectOnPlane(dirction, Vector3.up) - new Vector3(0, jumpspeed -9.81f, 0) * Time.deltaTime);
-        Debug.Log(jump.state);
+        characterController.Move(speed * Time.deltaTime * Vector3.ProjectOnPlane(dirction, Vector3.up) + new Vector3(0, gravity, 0) * Time.deltaTime);
 
-        if (jump.state == true & isJumping == false)
+        if (jump.state == true && isJumping == false)
         {
             isJumping = true;
-            Jumping();
+            gravity = -gravity;
+            StartCoroutine(Jumping());
         }
     }
 
-    void Jumping()
+    IEnumerator Jumping()
     {
-        while (characterController.isGrounded == false && jumpspeed <=  10f)
-        {
-            jumpspeed = 9.81f * Time.deltaTime + jumpspeed;
-        }
-        jumpspeed = 0;
+        yield return new WaitForSeconds(0.3f);
+        gravity = -gravity;
     }
 
 
